b3566b64c2c7d0bbebffcad0b265ce1ef0230801
Растов Владимир Алексеевич <rastov.va@devvlg.region.cbr.ru>
2017-08-03 13:05:42 +0300

    Создан модуль для Castom Fild

---
+++ src/test/java/ut/ru/cbr/jira/plugins/jira/customfields/TestCastomFieldTest.java
@@ -0,0 +1,34 @@
+package ut.ru.cbr.jira.plugins.jira.customfields;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import ru.cbr.jira.plugins.jira.customfields.TestCastomField;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @since 3.5
+ */
+public class TestCastomFieldTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test(expected=Exception.class)
+    public void testSomething() throws Exception {
+
+        //TestCastomField testClass = new TestCastomField();
+
+        throw new Exception("TestCastomField has no tests!");
+
+    }
+
+}
